9bc629085a2a3a3e131148e4917bf5056234e2b4
Jones-Ere Alaere <joneserealaere@gmail.com>
2025-10-04 10:18:06 +0100

    .

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -87,7 +87,7 @@ export default function ProjectsPage() {
         </div>
         <div className="container relative z-10">
           <div className="max-w-4xl mx-auto text-center">
-            <div className="inline-block bg-mid-green/20 border border-mid-green/30 px-4 py-2 rounded mb-6">
+            <div className="inline-block bg-mid-green/20 border border-mid-green/30 px-4 py-20 rounded mb-6">
               <span className="!text-white font-bold text-md uppercase tracking-wide">Project Portfolio</span>
             </div>
             <h1 className="!text-white mb-6 text-2xl md:text-3xl font-bold">Proven Track Record Across Nigeria</h1>
